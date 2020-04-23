5d575c81e68157aa93532583a69eac71d7a10e62
Rena Auerbach <renaauerbach@gmail.com>
2020-04-22 21:31:57 -0500

    Updated Memory and Event rendering

--- src/components/ListEvents.js
+++ src/components/ListEvents.js
@@ -2,24 +2,19 @@ import React from 'react';
 
 import Wrapper from '../components/Wrapper';
 
-const ListEvents = ({ events }) => {
-   return (
-      <ul>
-         {events.map((event, index) => {
-            let text = event.text;
-            let info = { author: event.author, date: event.createdAt };
-            return (
-               <Wrapper
-                  wrap={index % 2 === 1 ? 'style1' : 'style2'}
-                  title={event.title}
-                  image={event.image}
-                  text={text}
-                  info={info}
-               />
-            );
-         })}
-      </ul>
-   );
-};
+const ListEvents = ({ events }) =>
+   events.map((event, index) => {
+      let text = event.text;
+      let info = { author: event.author, date: event.createdAt };
+      return (
+         <Wrapper
+            wrap={index % 2 === 1 ? 'style1' : 'style2'}
+            title={event.title}
+            image={event.image}
+            text={text}
+            info={info}
+         />
+      );
+   });
 
 export default ListEvents;
--- src/components/ListMemories.js
+++ src/components/ListMemories.js
@@ -2,24 +2,18 @@ import React from 'react';
 
 import Wrapper from '../components/Wrapper';
 
-const ListMemories = ({ memories }) => {
-   return (
-      <ul>
-         {memories.map((memory, index) => {
-            let text = memory.text;
-            let info = { author: memory.author, date: memory.createdAt };
-            return (
-               <Wrapper
-                  wrap={index % 2 === 1 ? 'style1' : 'style2'}
-                  title={memory.title}
-                  image={memory.image}
-                  text={text}
-                  info={info}
-               />
-            );
-         })}
-      </ul>
-   );
-};
+const ListMemories = ({ memories }) =>
+   memories.map((memory, index) => {
+      let info = { author: memory.author, date: memory.createdAt };
+      return (
+         <Wrapper
+            wrap={index % 2 === 1 ? 'style1' : 'style2'}
+            title={memory.title}
+            image={memory.image}
+            text={memory.text}
+            info={info}
+         />
+      );
+   });
 
 export default ListMemories;
