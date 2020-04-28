ea31f372726b0ef1bc9758c26832cf59ad141589
Rena Auerbach <renaauerbach@gmail.com>
2020-04-28 15:38:31 -0500

    Deployment configuration part 4

--- src/server/config/keys.js
+++
@@ -1,6 +0,0 @@
-MONGO_URI =
-   'mongodb://teamCalebUser:teamCalebPass@127.0.0.1:27017/team_caleb?authSource=team_caleb';
-
-module.exports = {
-   MONGO_URI: MONGO_URI,
-};
--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const fs = require('fs');
 require('dotenv').config();
 
 const memoryRouter = require('./api/memory');
@@ -22,10 +23,23 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB
-const db = require('./config/keys').MONGO_URI;
+let dbconf;
+// Check if the environment variable (NODE_ENV) is set to PRODUCTION
+if (process.env.NODE_ENV === 'PRODUCTION') {
+   // If so --> read configration from a file (using blocking file I/O)
+   const data = fs.readFileSync(path.join(__dirname, '../../config.json'));
+   // Parse the JSON configuration file and set the connection string appropriately
+   const conf = JSON.parse(data);
+   dbconf = conf.dbconf;
+} else {
+   // If NOT in PRODUCTION mode
+   dbconf =
+      'mongodb://teamCalebUser:teamCalebPass@127.0.0.1:27017/team_caleb?authSource=team_caleb';
+}
+
 mongoose.Promise = global.Promise;
 mongoose
-   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+   .connect(dbconf, { useNewUrlParser: true, useUnifiedTopology: true })
    .then(() => console.log(`Database connected successfully`))
    .catch((err) => console.log('Connection error: ', err));
 
@@ -35,11 +49,6 @@ app.use('/api/events', eventRouter);
 app.use('/api/contact', messageRouter);
 
 app.use((req, res, next) => {
-   // res.header('Access-Control-Allow-Origin', '*');
-   // res.header(
-   //    'Access-Control-Allow-Headers',
-   //    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-   // );
    next();
 });
 
